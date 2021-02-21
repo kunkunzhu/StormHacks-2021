743c488ce5f4dd7008d7921a084deb551b4fa2aa
avash007thapa <58122425+avash007thapa@users.noreply.github.com>
2021-02-20 21:48:07 -0800

    setting structure
    setting structure for transition from login to home screen

--- app/src/main/java/com/example/heartmony/MainActivity.java
+++ app/src/main/java/com/example/heartmony/MainActivity.java
@@ -3,6 +3,8 @@ package com.example.heartmony;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +12,14 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Button logInButton = findViewById(R.id.logInButton);
+
+        logInButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                // Toast is the message
+            }
+        });
     }
 }
