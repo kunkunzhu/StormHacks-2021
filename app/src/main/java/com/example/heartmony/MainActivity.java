fd12b2b0da83ed891a825a39e63e6b38e24b4cc9
klocquiao <klocquiao45@gmail.com>
2021-02-20 22:17:23 -0800

    new imports in MainActivity

--- app/src/main/java/com/example/heartmony/MainActivity.java
+++ app/src/main/java/com/example/heartmony/MainActivity.java
@@ -1,10 +1,19 @@
 package com.example.heartmony;
 
+import android.os.Bundle;
 import androidx.appcompat.app.AppCompatActivity;
-
+import android.util.Log;
 import android.os.Bundle;
 
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
+
 public class MainActivity extends AppCompatActivity {
+
+    private static final String TAG = "MainActivity";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
